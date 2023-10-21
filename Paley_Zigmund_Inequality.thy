dbb54c27874a6b54af516ba404a75b06b7779142
tanyongkiam <tanyongkiam@gmail.com>
2023-10-21 22:48:59 +0800

    add sketch of Paley-Zigmund in prob_space

---
+++ Paley_Zigmund_Inequality.thy
@@ -0,0 +1,96 @@
+theory Paley_Zigmund_Inequality
+  imports "HOL-Probability.Probability"
+begin
+
+context prob_space
+begin
+
+lemma paley_zigmund:
+  assumes rv: "random_variable borel Z"
+  assumes intZsq: "integrable M (\<lambda>z. (Z z)^2)"
+  assumes t: "\<theta> \<le> 1"
+  assumes Zpos: "\<And>z. z \<in> space M \<Longrightarrow> Z z \<ge> 0"
+  shows "
+    (variance Z + (1-\<theta>)^2 * (expectation Z)^2) * prob {z. Z z > \<theta> * expectation Z}
+    \<ge> (1-\<theta>)^2 * (expectation Z)^2"
+proof -
+  have intZ: "integrable M Z"
+    apply (subst square_integrable_imp_integrable[OF rv intZsq])
+    by auto
+
+  define eZ where "eZ = expectation Z"
+  have "eZ \<ge> 0"
+    unfolding eZ_def
+    using Bochner_Integration.integral_nonneg Zpos by blast
+
+  have "expectation (\<lambda>z. Z z - \<theta> * eZ) = expectation (\<lambda>z. Z z + (- \<theta> * eZ))"
+    by auto
+  moreover have "... = expectation Z + expectation (\<lambda>z. - \<theta> * eZ)"
+    apply (subst Bochner_Integration.integral_add)
+    using intZ by auto
+  moreover have "... = eZ + (- \<theta> * eZ)"
+    apply (subst lebesgue_integral_const)
+    using eZ_def prob_space by auto
+  ultimately have *: "expectation (\<lambda>z. Z z - \<theta> * eZ) = eZ - \<theta> * eZ"
+    by linarith
+
+  have ev: "{z. \<theta> * eZ < Z z} \<in> events"
+    sorry
+    (* should come from rv? *)
+
+  have ***: "eZ - \<theta> * eZ \<le>
+    expectation (\<lambda>z. (Z z - \<theta> * eZ) * indicat_real {z. Z z > \<theta> * eZ} z)"
+    unfolding *[symmetric]
+    apply (auto intro!: integral_mono) 
+    using intZ apply auto[1]
+    apply (auto intro!: integrable_real_mult_indicator simp add: intZ ev)[1]
+    unfolding indicator_def of_bool_def
+    by (auto simp add: mult_nonneg_nonpos2)
+
+  have "variance Z =
+     variance (\<lambda>z. (Z z - \<theta> * eZ))"
+    using "*" eZ_def by auto
+  moreover have "... =
+    expectation (\<lambda>z. (Z z - \<theta> * eZ)^2)
+    - (expectation (\<lambda>x. Z x - \<theta> * eZ))\<^sup>2"
+    apply (subst variance_eq)
+    by (auto simp add: intZ power2_diff intZsq)
+
+  moreover have "... = expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) - ((1-\<theta>)^2 * eZ^2)"
+    unfolding *
+    apply auto
+    by (metis left_diff_distrib mult_1 power_mult_distrib)
+  
+  ultimately have veq: "expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) = (variance Z + (1-\<theta>)^2 * eZ^2)"
+    by linarith
+
+  have "(1-\<theta>)^2 * (expectation Z)^2 = (eZ - \<theta> * eZ)^2"
+    unfolding eZ_def[symmetric]
+    by (metis mult.commute mult.right_neutral power_mult_distrib right_diff_distrib')
+
+  moreover have"... \<le>
+    (expectation (\<lambda>z. (Z z - \<theta> * eZ) * indicat_real {z. Z z > \<theta> * eZ} z))^2"
+    using ***  
+    by (smt (verit) \<open>0 \<le> eZ\<close> mult.commute mult_left_le power2_less_imp_less t)
+
+  moreover have "... \<le>
+    expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) * (expectation (\<lambda>z. indicat_real {z. Z z > \<theta> * eZ} z^2))"
+    sorry
+    (* Cauchy-Schwarz for E of product of two random variables *)
+
+  moreover have "... = 
+    expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) * expectation (\<lambda>z. indicat_real {z. Z z > \<theta> * eZ} z)"
+    by (metis indicator_simps(1) indicator_simps(2) one_power2 zero_eq_power2)
+  moreover have "... = expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) * prob {z. Z z > \<theta> * eZ}"
+    apply auto
+    using ev measure_space_inter by blast
+
+  ultimately have "(1-\<theta>)^2 * (expectation Z)^2 \<le>
+     expectation (\<lambda>z. (Z z - \<theta> * eZ)^2) * prob {z. Z z > \<theta> * eZ}" by auto
+  thus ?thesis
+    using eZ_def veq by fastforce
+qed
+
+end
+
+end
--- ROOT
+++ ROOT
@@ -4,6 +4,7 @@ session Concentration_Inequalities = "HOL-Probability" +
   options [timeout = 1200]
   theories
     Effron_Stein_Inequality
+    Paley_Zigmund_Inequality
   document_files
     "root.tex"
     "root.bib"
